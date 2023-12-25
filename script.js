3d5f1b6491e41f7fc3cfb1d1b752b7def3ec5282
bartonfunky <c.d.v.amos@gmail.com>
2023-12-25 12:30:10 +1100

    added

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ document.getElementById('rollButton').addEventListener('click', function() {
     dice.textContent = randomNumber; // Update text or image according to randomNumber
   }, 1000); // Set timeout according to animation duration
 });
+
+function rollDice() {
+    var dice = document.getElementById('dice');
+    dice.classList.add("dice-rolling");
+  
+    // Play sound
+    var tickSound = document.getElementById('tickSound');
+    tickSound.play();
+  
+    // Remove the animation class and set the final number after 3 seconds
+    setTimeout(() => {
+      dice.classList.remove("dice-rolling");
+      // set the final face of the dice based on a random number
+      dice.textContent = Math.floor(Math.random() * 6) + 1;
+    }, 3000);
+  }
+  
+  document.getElementById('rollButton').addEventListener('click', rollDice);
